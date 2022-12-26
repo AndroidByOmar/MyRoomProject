8e0fbceef4907e9616d58ef6a8d73cef04df8c8d
omarabdelsalm <93231975za@gmail.com>
2022-12-26 18:28:38 +0200

    Add RecyclerView and this option

---
+++ app/src/main/java/com/example/omar/myroomap/model/User.kt
@@ -0,0 +1,3 @@
+package com.example.omar.myroomap.model
+
+data class User(var name:String,var message:String,var image:Int)
---
+++ app/src/main/java/com/example/omar/myroomap/ui/userRecyclerView.kt
@@ -0,0 +1,6 @@
+package com.example.omar.myroomap.ui
+
+import androidx.recyclerview.widget.RecyclerView
+
+class userRecyclerView {
+}
